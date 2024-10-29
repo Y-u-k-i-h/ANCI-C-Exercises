6af015c9db4bd92a66d080daadab3398a984d871
R1CH13 <4rnold.richard@gmail.com>
2024-10-29 21:23:15 +0300

    third exercise

--- Exercise1-5.c
+++ Exercise1-5.c
@@ -6,6 +6,7 @@ main() {
     int fahr;
 
     printf("Farenheit\tCelcius\n");
-    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
-    printf("%3d\t\t\t%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    for (fahr = 300; fahr > 0; fahr = fahr - 20){
+        printf("%3d\t\t\t%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }
 }
